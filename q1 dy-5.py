8146a9330991af41a069037dfcadf6b886cecc56
pagidalamadeena <123723175+pagidalamadeena@users.noreply.github.com>
2023-02-03 22:33:53 +0530

    Add files via upload

---
+++ q1 dy-5.py
@@ -0,0 +1,10 @@
+s=input("Enter the string:")
+s1=s.split()
+n=len(s1)
+print("Number of words: ",n)
+print("Last word: ",s1[n-1], len(s1[n-1]))
+
+Output:
+Enter the string: Welcome to Python Programming
+Number of words:  4
+Last word:  Programming 11
